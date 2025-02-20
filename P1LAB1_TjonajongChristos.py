267e1f4d7009fd8d753c94a47d748efc4d22a032
tjonajoc7938 <tjonajoc7938@student.faytechcc.edu>
2025-02-20 12:48:55 -0500

    Add files via upload
    First submission

---
+++ P1LAB1_TjonajongChristos.py
@@ -0,0 +1,9 @@
+# Christos Tjonajong
+# 2/19/25
+# P1LAB1
+# Welcomes users to course
+print('Enter your first name:')
+firstName = input()
+print('Enter your last name:')
+lastName = input()
+print('Hello,', firstName, lastName, '! Welcome to CTI-110')
